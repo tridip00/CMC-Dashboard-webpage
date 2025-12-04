7b7483ef171c13d4db8b70cbbcdf81546291ec60
tridip00 <tridipkarmakar@psgroup.org.in>
2025-12-04 17:33:50 +0000

    update the deploy

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/CMC-Dashboard-webpage/',
   plugins: [react(),  tailwindcss()],
 })
+
 
 
